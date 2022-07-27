32f77921c3478d325970c1d56e05c2cb386d972a
Ildar <ildar9@list.ru>
2022-07-27 17:26:21 +0300

    work with card style

--- my-app/src/components/Beer/Beer.jsx
+++ my-app/src/components/Beer/Beer.jsx
@@ -3,19 +3,25 @@ import styles from './Beer.module.scss'
 
 export default function Beer({ beer }) {
 
-  console.log('пивас', beer)
+  const cutText = (text) => {
+    let result = '';
+    if (text.length > 140) {
+      result = text.slice(0,136) + '...';
+      return result
+    } else {
+      return text
+    }    
+  }
   
 
   return (
-    <div>
-      <div className="card" style={{ width: '300px', height: '600px' }}>
+      <div className={styles.beer}>
         <img src={ beer.image_url } className={ styles.beer__image } alt="Beer foto"/>
-        <div className="card-body">
-          <h5 className="card-title">{ beer.name }</h5>
-          <p className="card-text">{ beer.description }</p>
-          {/* <a href="#" class="btn btn-primary">Go somewhere</a> */}
+        <div className={ styles.body }>
+          <h5 className={ styles.body__title }>{ beer.name }</h5>
+          <p className={ styles.body__discription }>{ cutText(beer.description) }</p>
+          <a href="#" className={ styles.body__moreInfo }>Go somewhere</a>
         </div>
       </div>
-    </div>
   )
 }
